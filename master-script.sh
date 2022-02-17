b58e0570ffc2ce1f94018eba3853cafaa6a0aae8
Chris Rabe (AU) <chris.rabe14@gmail.com>
2022-02-18 02:31:48 +1000

    fix: passed in services as args

--- master-script.sh
+++ master-script.sh
@@ -3,6 +3,15 @@
 # Note:
 # This is the code you paste into your own repository!
 
+SERVICES="$1"
+
+if [ -z "${SERVICES}" ]
+then
+  echo "\033[1;31mServices must be provided (comma separated, no spaces)\033[0m"
+  echo "\033[1;31mUsage: ./add-pactum.sh \"service1,service2,service3\"\033[0m"
+  exit 1
+fi
+
 PACTUM_INSTALLER_REPO="https://github.com/chrisrabe/pactum-installer.git"
 PACTUM_INSTALLER_DIR="./pactum-installer"
 INSTALL_SCRIPT="install-pactum.sh"
@@ -13,7 +22,7 @@ mv "${PACTUM_INSTALLER_DIR}/${INSTALL_SCRIPT}" .
 chmod +x "./${INSTALL_SCRIPT}"
 
 # Run the installer
-./${INSTALL_SCRIPT}
+./${INSTALL_SCRIPT} "${SERVICES}"
 
 # Clean up everything
 rm -rf "./${INSTALL_SCRIPT}"
