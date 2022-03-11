03dab920ce7edcb0e830466016a294352a3f10cf
Chris Rabe (AU) <chris.rabe14@gmail.com>
2022-03-11 10:40:06 +1000

    fix: fixed ts issue with port

--- jest-files/global-setup.ts
+++ jest-files/global-setup.ts
@@ -4,5 +4,5 @@ process.env.PACTUM_PORT = '3000';
 process.env.PACTUM_URL = `http://localhost`;
 
 export default function name() {
-    mock.start(parseInt(process.env.PACTUM_PORT));
+    mock.start(parseInt(process.env.PACTUM_PORT as string));
 }
